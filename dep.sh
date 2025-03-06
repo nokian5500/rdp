b5f91d986b895671949cdcbdf019f5fa6548ae94
nokian5500 <nokian5500@gmail.com>
2025-03-06 09:30:23 +0300

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -1,9 +1,12 @@
-Install-Module -Name Az -AllowClobber -Force
+# Установка модуля Az, если он не установлен
+if (-not (Get-Module -ListAvailable -Name Az)) {
+    Install-Module -Name Az -AllowClobber -Force -Scope CurrentUser
+}
 
 # Убедитесь, что вы вошли в Azure
 Connect-AzAccount
 
-# Получаем список всех доступных локаций
+# Получаем список всех доступных локаций, где поддерживается Microsoft.Compute
 $locations = Get-AzLocation | Where-Object { $_.Providers -contains "Microsoft.Compute" } | Select-Object -ExpandProperty Location
 
 # Цикл по всем локациям
@@ -13,18 +16,24 @@ foreach ($location in $locations) {
     # Создаем уникальное имя группы ресурсов для каждой локации
     $resourceGroupName = "rg-vm-deploy-$location"
 
-    # Создаем группу ресурсов
-    New-AzResourceGroup -Name $resourceGroupName -Location $location
+    try {
+        # Создаем группу ресурсов
+        Write-Host "Создание группы ресурсов: $resourceGroupName"
+        New-AzResourceGroup -Name $resourceGroupName -Location $location -Force
 
-    # Развертываем шаблон
-    $deployment = New-AzResourceGroupDeployment `
-        -ResourceGroupName $resourceGroupName `
-        -TemplateUri "https://raw.githubusercontent.com/nokian5500/rdp/refs/heads/main/azuredeploy.json" `
-        -Location $location
+        # Развертываем шаблон
+        Write-Host "Развертывание шаблона в группе ресурсов: $resourceGroupName"
+        $deployment = New-AzResourceGroupDeployment `
+            -ResourceGroupName $resourceGroupName `
+            -TemplateUri "https://raw.githubusercontent.com/nokian5500/rdp/refs/heads/main/azuredeploy.json" `
+            -Location $location
 
-    # Выводим учетные данные
-    $credentials = $deployment.Outputs.credentials.Value
-    Write-Host "Учетные данные для $location: $credentials"
+        # Выводим учетные данные
+        $credentials = $deployment.Outputs.credentials.Value
+        Write-Host "Учетные данные для $location: $credentials"
+    } catch {
+        Write-Host "Ошибка при развертывании в локации $location: $_"
+    }
 }
 
-Write-Host "Развертывание завершено во всех локациях."
+Write-Host "Развертывание завершено во всех локациях." 
