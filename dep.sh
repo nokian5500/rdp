cc9a31a1083d4bec6446fb6d844c7a396e614524
nokian5500 <nokian5500@gmail.com>
2025-03-06 08:41:51 +0300

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -1,3 +1,30 @@
-var rggroup = $RANDOM
-az group create --name rggroup --location eastus
-az deployment group create --resource-group rggroup --template-file main.bicep --parameters adminPassword=rggroup
+Install-Module -Name Az -AllowClobber -Force
+
+# Убедитесь, что вы вошли в Azure
+Connect-AzAccount
+
+# Получаем список всех доступных локаций
+$locations = Get-AzLocation | Where-Object { $_.Providers -contains "Microsoft.Compute" } | Select-Object -ExpandProperty Location
+
+# Цикл по всем локациям
+foreach ($location in $locations) {
+    Write-Host "Развертывание в локации: $location"
+
+    # Создаем уникальное имя группы ресурсов для каждой локации
+    $resourceGroupName = "rg-vm-deploy-$location"
+
+    # Создаем группу ресурсов
+    New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+    # Развертываем шаблон
+    $deployment = New-AzResourceGroupDeployment `
+        -ResourceGroupName $resourceGroupName `
+        -TemplateUri "deploy.json" `
+        -Location $location
+
+    # Выводим учетные данные
+    $credentials = $deployment.Outputs.credentials.Value
+    Write-Host "Учетные данные для $location: $credentials"
+}
+
+Write-Host "Развертывание завершено во всех локациях."
